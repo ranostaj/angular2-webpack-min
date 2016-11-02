ebea936527e5861e9a3eba5e15d1c8530770d6ff
jan.ranostaj <jan.ranostaj@accenture.com>
2016-11-02 07:44:14 +0100

    Webpack 2 config

---
+++ app/main.ts
@@ -0,0 +1,10 @@
+/*
+ * Angular bootstraping
+ */
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app.module';
+
+
+
+platformBrowserDynamic().bootstrapModule(AppModule);
--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,53 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
 
 module.exports = {
 
-  context: __dirname + "/app",
+   context: __dirname + "/app",
   resolve: {
-    extensions: ['', '.ts', '.min.js', '.js', '.json', '.scss']
+    extensions: ['.ts', '.min.js', '.js', '.json', '.scss'],
+    modules: [__dirname + "/app", 'node_modules'],
   },
   entry: {
-    'app': './main.ts',
-    'polyfills' : './polyfills.ts'
+    'polyfills' : './polyfills.ts',
+    'vendors' : './vendors.ts',
+    'app': './main.ts'
   },
 
   output: {
     path: __dirname + '/build',
     filename: '[name].bundle.js',
-    publicPath: '/'
+    publicPath: 'http://localhost:9000'
   },
 
   module: {
-    loaders: [
-
+    rules: [
       {
         test: /\.ts$/,
-        loaders: ['awesome-typescript-loader', 'angular2-template-loader'],
+        loaders: ['awesome-typescript-loader','angular2-template-loader'],
         exclude: [/node_modules/]
+      },
+      {
+        test: /\.html$/,
+        loader: 'raw-loader'
       }
-
     ]
-
   },
 
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html',
+      title: 'Angular 2 - Webpack minimal',
       inject: 'body'
-    })
+    }),
+     new  webpack.optimize.CommonsChunkPlugin({
+         name: ['app', 'vendor', 'polyfills']
+     })
   ],
 
-
-  devServer: {
-    contentBase: './app'
-  }
-
+    devServer: {
+        outputPath: __dirname + "/build",
+        port: 9000
+    }
 }
